1d6e25b35826e59787c02ae31b056ce8ea47922b
Jhonatan da Costa <jhonatandacosta0509@gmail.com>
2018-06-21 11:59:03 -0300

    Aprendendo node

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+app.post('/', (req, res) => {
+    res.send('Recebi um POST');
+});
+
+app.get('/pudim', (req, res) => {
+    res.send({
+        sabor:'Leite',
+        cobertura:'caramelo',
+        frescura: 'uva passa',
+        txt: 'Olá!!!'
+        
+    });
+});
+
+
+app.listen(3000);
